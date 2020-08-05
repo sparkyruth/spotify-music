712872e1867323cb3c7e76da2d66f24b5afcf3c0
Alba Moreno <alba.moreno@bootcamp-upgrade.com>
2020-08-05 12:37:20 +0200

    create home module and route

--- spotify-app/src/app/app-routing.module.ts
+++ spotify-app/src/app/app-routing.module.ts
@@ -2,7 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { // default route => /home
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  { // route => /home
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
