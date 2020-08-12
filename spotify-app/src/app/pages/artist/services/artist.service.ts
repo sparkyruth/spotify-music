fd19275821f87fd308e7eb7e3f4072dc88df78b6
Alba Moreno <alba.moreno@bootcamp-upgrade.com>
2020-08-12 12:07:47 +0200

    refactorization

--- spotify-app/src/app/pages/artist/services/artist.service.ts
+++ spotify-app/src/app/pages/artist/services/artist.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { map, catchError } from 'rxjs/operators';
 
 // Services
 import { GlobalService } from 'src/app/services/global.service';
@@ -13,19 +12,6 @@ export class ArtistService {
   // get artist info
   public getArtist(artistId: string): Observable<any> {
     const artistUrl: string = `artists/${ artistId }`;
-
-    return this.globalService.getQuery(artistUrl).pipe(
-      map((res: any) => {
-        if (!res) {
-          throw new Error('Value expected!');
-        } else {
-          // TO-DO: tipar
-          console.log('res:', res);
-          return res;
-        }
-      }),
-      catchError((err) => {
-        throw new Error(err.message);
-      }));
+    return this.globalService.getQuery(artistUrl);
   }
 }
