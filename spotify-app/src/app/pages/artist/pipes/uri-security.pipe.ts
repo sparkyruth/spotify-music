8741da2bd806014d4b4887a19305de7dcdb54845
Alba Moreno <alba.moreno@bootcamp-upgrade.com>
2020-08-12 16:25:57 +0200

    modify pipe uri security !

--- spotify-app/src/app/pages/artist/pipes/uri-security.pipe.ts
+++ spotify-app/src/app/pages/artist/pipes/uri-security.pipe.ts
@@ -9,7 +9,8 @@ export class UriSecurityPipe implements PipeTransform {
   constructor( private domSanitizer: DomSanitizer){ /*empty*/ }
 
   // receive uri and verifies security
-  transform( value: string, url: string): any {
+  transform( value: string): any {
+    const url = 'https://open.spotify.com/embed?uri=';
     return this.domSanitizer.bypassSecurityTrustResourceUrl( url + value );
   }
 
