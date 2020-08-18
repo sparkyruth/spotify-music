c7b66d3eb6594f7bcfa1414f8686a1d38b71005a
Alba Moreno <alba.moreno@bootcamp-upgrade.com>
2020-08-18 10:55:48 +0200

    add float button in artist view

--- spotify-app/src/app/pages/artist/artist-component/artist.component.ts
+++ spotify-app/src/app/pages/artist/artist-component/artist.component.ts
@@ -83,4 +83,8 @@ export class ArtistComponent implements OnInit {
   public seeMoreTracks(): void {
     this.moreTracks = !this.moreTracks;
   }
+
+  public scrollTo(elementId: string): void {
+    document.getElementById(elementId).scrollIntoView();
+  }
 }
